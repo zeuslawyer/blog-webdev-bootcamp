5feb760f5695feb96952c9d6aef564d38ca4e272
zubin aka zeuslawyer <zubin.pratap@gmail.com>
2018-08-29 15:23:51 +1000

    IMPORTANT.  diff between $.click() and $.on() and how to attach listener to dynamically created elements

--- Todo-List-Project/assets/js/todos.js
+++ Todo-List-Project/assets/js/todos.js
@@ -1,24 +1,42 @@
 // alert('connected'); 
 
 // click listener for completed todo items
-$('li').click(function(){
+$('ul#ul-main').on('click', 'li', function(){
 	$(this).toggleClass('todo-completed');
 })
 
-//alternate row shading
-$('li:even').css('background', '#00ffff');
-$('li:odd').css('background', '#bfff00');
-
-// fadeout deleted elems. propogate to parent, then remove parent 
+// click listener - fadeout deleted elems. propogate to parent, then remove parent 
 // (nb: note the $(this) refers to different objects - first is span, second is span's parent, li !!)
-$('span').click(function(event){
-	$(this).parent().fadeOut(1500, function(){
+$('ul#ul-main').on('click', 'span', function(event){
+	console.log($(this));
+	$(this).parent().fadeOut(800, function(){
+		console.log($(this));
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+//input listener - when enter pressed collect value and add to TODO list
+$("input[type='text']").keypress(function(event){
+	// log(event.which, ' was pressed!' + ' aint it dandy');
+	if(event.which === 13) {
+		var inputText = $(this).val();  //collect text input
+		$('ul#ul-main').append('<li><span>X</span> ' + inputText + '</li>');  //append it to the ul and create new li elem
+		$(this).val(''); //reset input field to blank text
+	}
 })
 
+
+// //alternate row shading
+// $('li:even').css('background', '#00ffff');
+// // $('li:odd').css('background', '#bfff00');
+// $('ul').children().change(function(){
+// 	log('changed')
+// 	$('li:even').css('background', '#00ffff');
+// 	$('li:odd').css('background', '#bfff00');
+// });
+
 // console.log helper
-function log(anything) {
-	console.log(anything);
+function log(...args) {
+	console.log(...args);
 }
