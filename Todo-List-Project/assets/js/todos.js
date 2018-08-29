22608a8c6d5f4ab170b5dd931ce60ae69293d84f
zubin aka zeuslawyer <zubin.pratap@gmail.com>
2018-08-29 14:19:13 +1000

    implement delete icon fadeout and element removal. stopPropogate() implemented on child, and remove/fadeout on parent.

--- Todo-List-Project/assets/js/todos.js
+++ Todo-List-Project/assets/js/todos.js
@@ -2,5 +2,23 @@
 
 // click listener for completed todo items
 $('li').click(function(){
-  $(this).toggleClass('todo-completed');
+	$(this).toggleClass('todo-completed');
 })
+
+//alternate row shading
+$('li:even').css('background', '#00ffff');
+$('li:odd').css('background', '#bfff00');
+
+// fadeout deleted elems. propogate to parent, then remove parent 
+// (nb: note the $(this) refers to different objects - first is span, second is span's parent, li !!)
+$('span').click(function(event){
+	$(this).parent().fadeOut(1500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
+
+// console.log helper
+function log(anything) {
+	console.log(anything);
+}
