441446389f0bbb40971fd92c0ed8dced4da74521
zubin aka zeuslawyer <zubin.pratap@gmail.com>
2018-09-02 10:01:33 +1000

    add conditional logic with a new posts.ejs template. DIFFERENCE BETWEEN <%= and <%

--- node_express_apps/firstExpressApp/app.js
+++ node_express_apps/firstExpressApp/app.js
@@ -2,6 +2,13 @@ var express = require('express');
 const app = express();
 
 
+let posts = [
+    {title: 'Zubin missies Rowie', author: 'Zubin Pratap'},
+    {title: 'I love to code', author: 'Zubin Pratap'},
+    {title: 'I sometimes worry about the future', author: 'Zubin Pratap'},
+    {title: 'Frankie goes to Hollywood', author: 'Zp Keshavar'}
+]
+
 app.get('/', function(req, res){
     // res.send('<h1> WELCOME TO THE HOME PAGE</h1><p> <strong> We are glad you\'re here!</strong></p>'); 
     res.render('home.ejs', {url: req.url});
@@ -17,7 +24,11 @@ app.get('/bye', function(req, res){
     res.render('home.ejs', {url: req.url, goodbye: true});
 });
 
-app.get('/:param1/testing-params/:param2', function(req, res){
+app.get('/posts', function(req, res){
+    res.render('posts.ejs', {posts:posts})  
+});
+
+app.get('/:param1/pathvar /:param2', function(req, res){
     var param1 = req.params.param1;
     var param2= req.params.param2;
     res.send(`YOU USED ROUTE PARAMS TO GET HERE! Specifically, you used ${param1} as param1 and ${param2} as param2, to make the url ${req.url}!`);
@@ -30,6 +41,7 @@ app.get('*', function(req, res){
 
 
 // SERVER RUNNING
-app.listen(3000, ()=>{
-    console.log('server running on port 3000...');
+let port = process.env.PORT || 3000
+app.listen(port, ()=>{
+    console.log(`server running on port ${port}...`);
 })
