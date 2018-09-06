a27bb40fdea870ab42b2e8bea1eb3fd8a69fc5ac
zubin aka zeuslawyer <zubin.pratap@gmail.com>
2018-09-06 16:51:06 +1000

    install ejs dependency. setup mongoDB locally and connect to it

--- blogAppRESTful/app.js
+++ blogAppRESTful/app.js
@@ -4,8 +4,21 @@ const express = require('express'),
 
 const app = express()
 
+app.use(express.static('public'));  //serve static assets in public dir
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost/UdemyWebDev", function(err){
+    if(err){
+        console.log('DB CONNECTION ERROR!')
+    } else {
+        console.log('\n>> DB CONNECTED\n');
+    }
+});
+
+
+
 // HOME
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
     res.send('This is the Home Page.');
 });
 
