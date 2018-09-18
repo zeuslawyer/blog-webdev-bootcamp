dbab8b7950708c1ae754c2d5731da4ebe91b8786
zubin aka zeuslawyer <zubin.pratap@gmail.com>
2018-09-18 15:17:39 +1000

    create MIDDLEWARE module. Use Object Destructuring to import into app.js

--- blogAppRESTful/app.js
+++ blogAppRESTful/app.js
@@ -8,7 +8,8 @@ const express = require('express'),
       passport = require('passport'),
       localStrategy = require('passport-local'),
       User = require('./models/users.js'),
-      expressSession= require('express-session')
+      expressSession= require('express-session'),
+      {isUserAuthenticated, checkBlogAuthor, checkCommentAuthor, viewsData} = require('./middleware/functions.js')  
 
 const dotenv = require('dotenv').config();
     
@@ -159,7 +160,7 @@ app.get('/blogs/:id/edit', isUserAuthenticated, checkBlogAuthor, (req, res, next
 })
 
 //UPDATE BLOG - update db and redirect to show page for that updated blog
-app.put('/blogs/:id',isUserAuthenticated, checkBlogAuthor, (req, res, next)=>{
+app.put('/blogs/:id', isUserAuthenticated, checkBlogAuthor, (req, res, next)=>{
     // res.send('PUT METHOD AND UPDATE ROUTE WORKING');
     //sanitize:
     req.body.blog.body = req.sanitize(req.body.blog.body);
@@ -317,65 +318,6 @@ app.delete ('/blogs/:id/comments/:commId', isUserAuthenticated, checkCommentAuth
     });
 });
 
-//========================
-// MIDDLEWARE
-//========================
-
-function isUserAuthenticated(req, res, next) {
-    // console.log('=======\n' +req.path + '\n') 
-    /*
-        store the current req path in the session so it can be returned to  
-        see : https://stackoverflow.com/questions/13335881/redirecting-to-previous-page-after-authentication-in-node-js-using-passport-js
-    */
-    req.session.returnPath = req.path
-
-    if (req.isAuthenticated()){
-        return next();
-    }
-    console.log('user not signed in --redirected to login page, and then returnPath may be used')
-    res.redirect('/login')
-}
-
-function checkBlogAuthor(req, res, next) {
-    Blog.findById(req.params.id, (err, retrievedBlog)=> {
-        if (err)  {
-            res.send(" DB retrieve for Blog object didnt work. Could not verify user and author");
-        } else {
-            // e stablish if authenticated user is authorised to edit/delete etc
-            if ( retrievedBlog.author.id && retrievedBlog.author.id.equals(req.user._id)) {
-                // res.render('edit.ejs', {blog: retrievedBlog})
-                next();
-            } else {
-                // res.send ('you are not authorised to do this.')
-                console.log(`${req.user.displayName}, you are not authorised to do this to ${retrievedBlog}.`)
-                res.redirect('back');
-            }
-        }
-    });
-}
-
-function checkCommentAuthor(req, res, next) {
-    Comment.findById(req.params.commId, (err, retrievedComment)=> {
-        if (err)  {
-            res.send(" DB retrieve for Comment object didnt work. Could not verify user and authorship");
-        } else {
-            // console.log(retrievedComment)
-            // establish if authenticated user is authorised to edit/delete etc
-            if ( req.user && retrievedComment.author.id.equals(req.user._id)) {
-                next();
-            } else {
-                // console.log(`${req.user.displayName}, you are not authorised to do this to ${retrievedComment}.`)
-                res.send ('you are not authorised to edit/delete comments.')
-            }
-        }
-    });
-}
-
-function viewsData(req, res, next){
-    res.locals.currentUser = req.user;  //if not logged in, then is undefined
-    res.locals.pathData = req.path; //demo to show how data gets passed- each route shows  up on upper right
-    next();
-}
 
 //========================  
 // SERVER START
---
+++ blogAppRESTful/middleware/functions.js
@@ -0,0 +1,68 @@
+const Comment = require('../models/comments');
+const User = require('../models/users.js');
+const Blog = require('../models/blogs');
+
+//========================
+// MIDDLEWARE
+//========================
+
+middleware = {}
+
+middleware.isUserAuthenticated = (req, res, next) => {
+        // console.log('=======\n' +req.path + '\n') 
+        /*
+            store the current req path in the session so it can be returned to  
+            see : https://stackoverflow.com/questions/13335881/redirecting-to-previous-page-after-authentication-in-node-js-using-passport-js
+        */
+        req.session.returnPath = req.path
+
+        if (req.isAuthenticated()){
+            return next();
+        }
+        console.log('user not signed in --redirected to login page, and then returnPath may be used')
+        res.redirect('/login')
+    };
+
+middleware.checkBlogAuthor = (req, res, next) => {
+    Blog.findById(req.params.id, (err, retrievedBlog)=> {
+        if (err)  {
+            res.send(" DB retrieve for Blog object didnt work. Could not verify user and author");
+        } else {
+            // e stablish if authenticated user is authorised to edit/delete etc
+            if ( retrievedBlog.author.id && retrievedBlog.author.id.equals(req.user._id)) {
+                // res.render('edit.ejs', {blog: retrievedBlog})
+                next();
+            } else {
+                // res.send ('you are not authorised to do this.')
+                console.log(`${req.user.displayName}, you are not authorised to do this to ${retrievedBlog}.`)
+                res.redirect('back');
+            }
+        }
+    });
+};
+
+middleware.checkCommentAuthor = (req, res, next) => {
+    Comment.findById(req.params.commId, (err, retrievedComment)=> {
+        if (err)  {
+            res.send(" DB retrieve for Comment object didnt work. Could not verify user and authorship");
+        } else {
+            // console.log(retrievedComment)
+            // establish if authenticated user is authorised to edit/delete etc
+            if ( req.user && retrievedComment.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                // console.log(`${req.user.displayName}, you are not authorised to do this to ${retrievedComment}.`)
+                res.send ('you are not authorised to edit/delete comments.')
+            }
+        }
+    });
+};
+
+    middleware.viewsData = (req, res, next) => {
+        res.locals.currentUser = req.user;  //if not logged in, then is undefined
+        res.locals.pathData = req.path; //demo to show how data gets passed- each route shows  up on upper right
+        next();
+    };
+
+
+module.exports = middleware;
